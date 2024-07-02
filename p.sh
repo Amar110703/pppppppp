22af79f431fa63f38bfad354ed344eb06041c4cc
Amar110703 <utolu5484@gmail.com>
2024-07-02 19:25:17 +0700

    Update p.sh

--- p.sh
+++ p.sh
@@ -4,7 +4,7 @@
 RANDOM_DATA=$(openssl rand -hex 16)
 
 # Encrypt the random data using AES-256-CBC
-ENCRYPTED_DATA=$(echo "$RANDOM_DATA" | openssl enc -aes-256-cbc -pass pass:your_secret_password -base64)
+ENCRYPTED_DATA=$(echo "$RANDOM_DATA" | openssl enc -aes-256-cbc -pass pass:marz -base64)
 
 # Token is the encrypted data
 TOKEN="$ENCRYPTED_DATA"
